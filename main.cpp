62ef6dc9f8a997ef7b229b88e9987fb61fa487ed
Ronnie Geraghty <rgeraghty@microsoft.com>
2024-08-13 18:31:04 -0700

    Init CMakeToolsExtensionIssue branch

--- main.cpp
+++ main.cpp
@@ -1,15 +1,6 @@
-#include <azure/identity.hpp> 
-#include <azure/keyvault/secrets.hpp> 
 #include <iostream>
 
-using namespace Azure::Security::KeyVault::Secrets;
-
-int main(){
-    std::cout<<"Starting Program!"<<std::endl;
-
-    auto const keyVaultUrl = std::getenv("AZURE_KEYVAULT_URL");
-
-    auto credential = std::make_shared<Azure::Identity::DefaultAzureCredential>();
-
-    SecretClient secretClient(keyVaultUrl, credential);
+int main() {
+    std::cout << "Hello World!";
+    return 0;
 }
--- toolchain.cmake
+++
@@ -1,3 +0,0 @@
-set(VCPKG_TARGET_TRIPLET "x64-mingw-dynamic")
-set(CMAKE_C_COMPILER "C:/Users/rgeraghty/mingw64/bin/gcc.exe")
-set(CMAKE_CXX_COMPILER "C:/Users/rgeraghty/mingw64/bin/g++.exe")
